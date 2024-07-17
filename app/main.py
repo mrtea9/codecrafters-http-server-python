1aa69a7cfceb14c0427b1a5c6e4f729beed52d56
mrtea9 <ionmeriacri111@gmail.com>
2024-07-17 16:35:50 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -10,13 +10,14 @@ def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     request = server_socket.accept()[0]
-    request_target = request.recv(2028).split()[1]
-    if request_target != b'/':
-        request.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        print(request_target)
-    else:
-        request.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        print(request_target)
+    request_target = request.recv(2028).decode().split("\r\n")
+    print(request_target)
+    # if request_target != b'/':
+    #     request.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    #     print(request_target)
+    # else:
+    #     request.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    #     print(request_target)
 
 
 if __name__ == "__main__":
