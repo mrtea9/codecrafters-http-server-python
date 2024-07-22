091c7c7930c431261f428a555bcd9af752385fba
mrtea9 <ionmeriacri111@gmail.com>
2024-07-22 16:28:22 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -11,9 +11,33 @@ NOTFOUND_RESPONSE = f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
 def threaded(c, addr):
     print(f"Handling new connection from {addr}")
     message = c.recv(1024).decode()
+    response = OK_RESPONSE
     if message:
-        print(message)
-    c.send(OK_RESPONSE)
+        request_body = message.split("\r\n")
+        get_body = request_body[0].split()
+        endpoint_body = get_body[1].split("/")
+
+        if endpoint_body[1] == 'echo':
+            endpoint_string = endpoint_body[2]
+            length = len(endpoint_string)
+            response = f"HTTP/1.1 200 OK\r\n" \
+                       f"Content-Type: text/plain\r\n" \
+                       f"Content-Length: {length}\r\n\r\n" \
+                       f"{endpoint_string}".encode()
+        elif endpoint_body[1] == 'user-agent':
+            user_agent_body = request_body[2].split()
+            user_agent_string = user_agent_body[1]
+            length = len(user_agent_string)
+            response = f"HTTP/1.1 200 OK\r\n" \
+                       f"Content-Type: text/plain\r\n" \
+                       f"Content-Length: {length}\r\n\r\n" \
+                       f"{user_agent_string}".encode()
+        elif endpoint_body[1] == '':
+            response = OK_RESPONSE
+        else:
+            response = NOTFOUND_RESPONSE
+
+    c.send(response)
     c.close()
 
 
