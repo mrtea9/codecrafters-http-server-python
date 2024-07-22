6fc82fede736dcd72218b1fdad4d6f4b72fa0cfc
mrtea9 <ionmeriacri111@gmail.com>
2024-07-22 17:19:02 +0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -19,41 +19,43 @@ def handle_client(client_socket, addr):
         method_body = request_body[0].split()
         method = method_body[0]
         endpoint_body = method_body[1].split("/")
-        print(method)
 
-        if endpoint_body[1] == 'echo':
-            endpoint_string = endpoint_body[2]
-            length = len(endpoint_string)
-            response = f"HTTP/1.1 200 OK\r\n" \
-                       f"Content-Type: text/plain\r\n" \
-                       f"Content-Length: {length}\r\n\r\n" \
-                       f"{endpoint_string}".encode()
-        elif endpoint_body[1] == 'user-agent':
-            user_agent_body = request_body[2].split()
-            user_agent_string = user_agent_body[1]
-            length = len(user_agent_string)
-            response = f"HTTP/1.1 200 OK\r\n" \
-                       f"Content-Type: text/plain\r\n" \
-                       f"Content-Length: {length}\r\n\r\n" \
-                       f"{user_agent_string}".encode()
-        elif endpoint_body[1] == 'files':
-            endpoint_string = endpoint_body[2]
-            path_file = sys.argv[2] + endpoint_string
-
-            try:
-                with open(path_file, 'r') as file:
-                    content = file.read()
-                length = os.path.getsize(path_file)
+        if method == 'GET':
+            if endpoint_body[1] == 'echo':
+                endpoint_string = endpoint_body[2]
+                length = len(endpoint_string)
                 response = f"HTTP/1.1 200 OK\r\n" \
-                           f"Content-Type: application/octet-stream\r\n" \
+                           f"Content-Type: text/plain\r\n" \
                            f"Content-Length: {length}\r\n\r\n" \
-                           f"{content}".encode()
-            except FileNotFoundError:
+                           f"{endpoint_string}".encode()
+            elif endpoint_body[1] == 'user-agent':
+                user_agent_body = request_body[2].split()
+                user_agent_string = user_agent_body[1]
+                length = len(user_agent_string)
+                response = f"HTTP/1.1 200 OK\r\n" \
+                           f"Content-Type: text/plain\r\n" \
+                           f"Content-Length: {length}\r\n\r\n" \
+                           f"{user_agent_string}".encode()
+            elif endpoint_body[1] == 'files':
+                endpoint_string = endpoint_body[2]
+                path_file = sys.argv[2] + endpoint_string
+
+                try:
+                    with open(path_file, 'r') as file:
+                        content = file.read()
+                    length = os.path.getsize(path_file)
+                    response = f"HTTP/1.1 200 OK\r\n" \
+                               f"Content-Type: application/octet-stream\r\n" \
+                               f"Content-Length: {length}\r\n\r\n" \
+                               f"{content}".encode()
+                except FileNotFoundError:
+                    response = NOTFOUND_RESPONSE
+            elif endpoint_body[1] == '':
+                response = OK_RESPONSE
+            else:
                 response = NOTFOUND_RESPONSE
-        elif endpoint_body[1] == '':
-            response = OK_RESPONSE
-        else:
-            response = NOTFOUND_RESPONSE
+        elif method == "POST":
+            print(method_body)
 
     client_socket.send(response)
     client_socket.close()
